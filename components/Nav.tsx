1ad8da73d68aa703f11451b24f526a90b558cbc0
tc1421 <tc1421@ic.ac.uk>
2023-05-21 21:08:07 +0900

    expandable context menu added

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -19,18 +19,35 @@ const Nav = () => {
     else setShowFull(true);
   }
 
+  const fullMenuClasses = "";
   return (
-    <nav className='text-white bg-black fixed bottom-0 left-0 w-full z-10 flex flex-row justify-between items-center h-16 px-4'>
-        <button className='' onClick={toggleShowFull}><Image src="/assets/menu_hamburger.svg" alt="toggle menu" width={30} height={30}/></button>
+    <nav className= {`text-white bg-black fixed bottom-0 left-0 z-10 flex flex-row justify-between items-center h-16 px-4 rounded-full m-4 ${ showFull ? 'w-[calc(100%-2rem)]' : ''}`}>
+        <button className='' onClick={toggleShowFull}>
+            <Image src="/assets/menu_hamburger.svg" alt="toggle menu" width={30} height={30}/>
+        </button>
 
-        <Link href="/profile">
-              <Image 
-                src="/assets/user1.svg"
-                width={37} height={37}
-                className="rounded-full"
-                alt="profile"
-              />
-        </Link>
+        {showFull ? (
+            <div className='flex flex-grow flex-row items-center justify-between ml-16'>
+                <Link href="/posts">
+                    Posts
+                </Link>
+                <Link href="/analytics">
+                    Analytics
+                </Link>
+                
+                <Link href="/profile">
+                    <Image 
+                    src="/assets/user1.svg"
+                    width={37} height={37}
+                    className="rounded-full"
+                    alt="profile"
+                    />
+                </Link>
+                
+            </div>
+        ) :
+        (<></>)}
+        
     </nav>
   )
 }
