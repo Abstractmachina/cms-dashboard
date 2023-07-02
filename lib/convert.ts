314618c3cda26b1078ddb11d78e2eae223fdb4ba
tc1421 <tc1421@ic.ac.uk>
2023-07-03 00:50:39 +0900

    post conversion from json added. some validation still required

---
+++ lib/convert.ts
@@ -0,0 +1,35 @@
+import { IPost } from "@/models/post.model";
+import mongoose, { Types } from "mongoose";
+// import {ObjectId} from 'mongodb';
+
+export default class Convert {
+    public static toPost(json:any) : IPost | null {
+        
+        // validation
+        if (!json["author"] || !json["published"] || !json["title"] || !json["content"] || !json["category"] || !json["lastEdited"] || !json["tags"]) {
+            throw new Error("Invalid JSON");
+        }
+        
+        // if (!mongoose.isObjectIdOrHexString(json["author"]))
+        //     throw new Error("Invalid Author");
+
+
+        let location = (json["location"]) ? json["location"]  : undefined;
+
+        const publishedDate = (json["publishedDate"]) ? new Date(json["publishedDate"]) : undefined;
+        
+        const result : IPost= {
+            author: json["author"],
+            published: json["published"],
+            title: json["title"],
+            content: json["content"],
+            category: json["category"],
+            lastEdited: new Date(json["lastEdited"]),
+            tags: json["tags"],
+            location,
+            publishedDate,
+        } 
+
+        return result;
+    }
+}
